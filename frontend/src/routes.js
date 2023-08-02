623673142f111e78fd6d57820cbfd8c75b3f6299
Elan Lin <elan00001@gmail.com>
2023-08-02 23:21:12 +0800

    remove useless components

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -21,23 +21,4 @@ export const Routes = {
     Supplierssettings: { path: "/supplierssettings" },//供應商基本資料設定
     BeginningInventorysettings: { path: "/beginninginventorysettings" },//期初存貨設定
 
-    // components
-    Accordions: { path: "/components/accordions" },
-    Alerts: { path: "/components/alerts" },
-    Badges: { path: "/components/badges" },
-    Widgets: { path: "/widgets" },
-    Breadcrumbs: { path: "/components/breadcrumbs" },
-    Buttons: { path: "/components/buttons" },
-    Forms: { path: "/components/forms" },
-    Modals: { path: "/components/modals" },
-    Navs: { path: "/components/navs" },
-    Navbars: { path: "/components/navbars" },
-    Pagination: { path: "/components/pagination" },
-    Popovers: { path: "/components/popovers" },
-    Progress: { path: "/components/progress" },
-    Tables: { path: "/components/tables" },
-    Tabs: { path: "/components/tabs" },
-    Tooltips: { path: "/components/tooltips" },
-    Toasts: { path: "/components/toasts" },
-    WidgetsComponent: { path: "/components/widgets" }
 };
